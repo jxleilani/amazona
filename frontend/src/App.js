ada1c3321baa02243c486b339c2fee2eff137b9b
Joy Meredith <joy@virtualpowersystems.com>
2021-11-03 22:24:23 -0400

    create react app

---
+++ frontend/src/App.js
@@ -0,0 +1,194 @@
+import React from "react";
+
+function App() {
+  return (
+    <div class="grid-container">
+      <header class="row">
+        <div>
+          <a class="brand" href="index.html">
+            amazona
+          </a>
+        </div>
+        <div>
+          <a href="cart.html">Cart</a>
+          <a href="signin.html">Sign In</a>
+        </div>
+      </header>
+      <main>
+        <div class="row center">
+          <div class="card">
+            <a href="product.html">
+              <img class="medium" src="./images/product-1.jpg" alt="product" />
+            </a>
+            <div class="card-body">
+              <a href="product.html">
+                <h2>Nike Slim Shirts</h2>
+              </a>
+              <div class="rating">
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+              </div>
+            </div>
+            <div class="price">$120</div>
+          </div>
+          <div class="card">
+            <a href="product.html">
+              <img class="medium" src="./images/product-1.jpg" alt="product" />
+            </a>
+            <div class="card-body">
+              <a href="product.html">
+                <h2>Nike Slim Shirts</h2>
+              </a>
+              <div class="rating">
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+              </div>
+            </div>
+            <div class="price">$120</div>
+          </div>
+          <div class="card">
+            <a href="product.html">
+              <img class="medium" src="./images/product-1.jpg" alt="product" />
+            </a>
+            <div class="card-body">
+              <a href="product.html">
+                <h2>Nike Slim Shirts</h2>
+              </a>
+              <div class="rating">
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+              </div>
+            </div>
+            <div class="price">$120</div>
+          </div>
+          <div class="card">
+            <a href="product.html">
+              <img class="medium" src="./images/product-1.jpg" alt="product" />
+            </a>
+            <div class="card-body">
+              <a href="product.html">
+                <h2>Nike Slim Shirts</h2>
+              </a>
+              <div class="rating">
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+              </div>
+            </div>
+            <div class="price">$120</div>
+          </div>
+          <div class="card">
+            <a href="product.html">
+              <img class="medium" src="./images/product-1.jpg" alt="product" />
+            </a>
+            <div class="card-body">
+              <a href="product.html">
+                <h2>Nike Slim Shirts</h2>
+              </a>
+              <div class="rating">
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star-o"></i>
+                </span>
+              </div>
+            </div>
+            <div class="price">$120</div>
+          </div>
+          <div class="card">
+            <a href="product.html">
+              <img class="medium" src="./images/product-1.jpg" alt="product" />
+            </a>
+            <div class="card-body">
+              <a href="product.html">
+                <h2>Nike Slim Shirts</h2>
+              </a>
+              <div class="rating">
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star-half-o"></i>
+                </span>
+              </div>
+            </div>
+            <div class="price">$120</div>
+          </div>
+        </div>
+      </main>
+      <footer class="row center">All rights reserved</footer>
+    </div>
+  );
+}
+
+export default App;
