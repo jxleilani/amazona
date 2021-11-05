30b055b5428fb37cb4423fc738e6b241d8955eab
Joy Meredith <joy@virtualpowersystems.com>
2021-11-04 21:47:27 -0400

    add routes to / and product

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,36 @@
 import React from "react";
-import Product from "./components/Product";
-import data from "./data";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
-    <div className='grid-container'>
-      <header className='row'>
-        <div>
-          <a className='brand' href='/'>
-            amazona
-          </a>
-        </div>
-        <div>
-          <a href='/cart'>Cart</a>
-          <a href='/signin'>Sign In</a>
-        </div>
-      </header>
-      <main>
-        <div className='row center'>
-          {data.products.map((product) => (
-            <Product key={product._id} product={product} />
-          ))}
-        </div>
-      </main>
-      <footer className='row center'>All rights reserved</footer>
-    </div>
+    <Router>
+      <div className='grid-container'>
+        <header className='row'>
+          <div>
+            <a className='brand' href='/'>
+              amazona
+            </a>
+          </div>
+          <div>
+            <a href='/cart'>Cart</a>
+            <a href='/signin'>Sign In</a>
+          </div>
+        </header>
+        <main>
+          <Switch>
+            <Router exact path='/'>
+              <HomeScreen />
+            </Router>
+            <Route path='/product/:id'>
+              <ProductScreen />
+            </Route>
+          </Switch>
+        </main>
+        <footer className='row center'>All rights reserved</footer>
+      </div>
+    </Router>
   );
 }
 
