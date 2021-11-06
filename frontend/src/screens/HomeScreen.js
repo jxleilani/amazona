d80731562daf2942c44f67e1884130a1df8e8210
Joy Meredith <joy@virtualpowersystems.com>
2021-11-06 17:21:12 -0400

    swap to get products from axios

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,16 +1,41 @@
-import React from 'react'
-import data from '../data'
-import Product from '../components/Product'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Product from "../components/Product";
 
 function HomeScreen() {
-  return (
-    <div className='row center'>
-    {data.products.map((product) => (
-      <Product key={product._id} product={product} />
-    ))}
-  </div>
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const { data } = await axios.get("/api/products");
+        setLoading(false);
+        setProducts(data);
+      } catch (err) {
+        setError(err.message);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
 
-  )
+  return (
+    <div>
+      {loading ? (
+        <LoadingBox />
+      ) : error ? (
+        <MessageBox>{error}</MessageBox>
+      ) : (
+        <div className='row center'>
+          {products.map((product) => (
+            <Product key={product._id} product={product} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default HomeScreen
+export default HomeScreen;
