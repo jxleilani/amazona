9ce3b96dfade7f45a75ca01e6d95c8d3b5141643
Joy Meredith <joy@virtualpowersystems.com>
2021-12-09 21:48:31 -0500

    added /api/products/:id and returns to ProductScreen

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,22 @@ import data from "./data.js"; // make sure to include .js
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Server is ready");
+app.get("/api/products/:id", (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product not found" });
+  }
 });
 app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
+app.get("/", (req, res) => {
+  res.send("Server is ready");
+});
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Serve at http://localhost:${PORT}`);
 });
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,64 +1,85 @@
-import React from "react";
-import { useParams } from "react-router";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { detailsProduct } from "../actions/productActions";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
 import Rating from "../components/Rating";
-import data from "../data";
 
-function ProductScreen() {
+function ProductScreen(props) {
+  const dispatch = useDispatch();
   const params = useParams();
-  const product = data.products.find((p) => p._id === params.id);
-  if (!product) return <div>Product Not Found</div>;
+  const productId = params.id;
+  const productDetails = useSelector((state) => state.productDetails);
+  const { loading, error, product } = productDetails;
+
+
+  useEffect(() => {
+    dispatch(detailsProduct(productId));
+  }, [dispatch, productId]);
 
   return (
     <div>
-      <Link to='/'>Back to result</Link>
-      <div className='row top'>
-        <div className='col-2'>
-          <img className='large' src={product.image} alt={product.name} />
-        </div>
-        <div className='col-1'>
-          <ul>
-            <li>
-              <h1>{product.name}</h1>
-            </li>
-            <li>
-              <Rating rating={product.rating} numReviews={product.numReviews} />
-            </li>
-            <li>Price: {`$${product.price}`}</li>
-            <li>
-              Description:
-              <p>{product.description}</p>
-            </li>
-          </ul>
-        </div>
-        <div className='col-1'>
-          <div className='card card-body'>
-            <ul>
-              <li>
-                <div className='row'>
-                  <div>Price</div>
-                  <div className='price'>{`$${product.price}`}</div>
-                </div>
-              </li>
-              <li>
-                <div className='row'>
-                  <div>Status&nbsp;</div>
-                  <div>
-                    {product.countInStock > 0 ? (
-                      <span className='success'>In Stock</span>
-                    ) : (
-                      <span className='danger'>Unavailable</span>
-                    )}
-                  </div>
-                </div>
-              </li>
-              <li>
-                <button className='primary block'>Add to Cart</button>
-              </li>
-            </ul>
+      {loading ? (
+        <LoadingBox />
+      ) : error ? (
+        <MessageBox variant='danger'>{error}</MessageBox>
+      ) : (
+        <div>
+          <Link to='/'>Back to result</Link>
+          <div className='row top'>
+            <div className='col-2'>
+              <img className='large' src={product.image} alt={product.name} />
+            </div>
+            <div className='col-1'>
+              <ul>
+                <li>
+                  <h1>{product.name}</h1>
+                </li>
+                <li>
+                  <Rating
+                    rating={product.rating}
+                    numReviews={product.numReviews}
+                  />
+                </li>
+                <li>Price: {`$${product.price}`}</li>
+                <li>
+                  Description:
+                  <p>{product.description}</p>
+                </li>
+              </ul>
+            </div>
+            <div className='col-1'>
+              <div className='card card-body'>
+                <ul>
+                  <li>
+                    <div className='row'>
+                      <div>Price</div>
+                      <div className='price'>{`$${product.price}`}</div>
+                    </div>
+                  </li>
+                  <li>
+                    <div className='row'>
+                      <div>Status&nbsp;</div>
+                      <div>
+                        {product.countInStock > 0 ? (
+                          <span className='success'>In Stock</span>
+                        ) : (
+                          <span className='danger'>Unavailable</span>
+                        )}
+                      </div>
+                    </div>
+                  </li>
+                  <li>
+                    <button className='primary block'>Add to Cart</button>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
